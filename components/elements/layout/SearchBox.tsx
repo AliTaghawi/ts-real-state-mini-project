ce96ca6a99292b31d6dfe3d8f7f05c4bea3d9242
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-07 01:39:12 +0330

    creating UI of searchBox

---
+++ components/elements/layout/SearchBox.tsx
@@ -0,0 +1,12 @@
+import { IoMdSearch } from "react-icons/io";
+
+const SearchBox = () => {
+  return (
+    <div className="flex items-center gap-1 bg-neutral-100 border border-neutral-300 py-0.5 px-2.5 rounded-2xl">
+      <IoMdSearch className="text-xl" />
+      <input type="text" placeholder="جستجو در عنوان" className=" outline-0 w-[200px]" />
+    </div>
+  );
+};
+
+export default SearchBox;
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,14 +1,16 @@
-import MenuItem from "@/elements/MenuItem";
+import MenuItem from "@/elements/layout/MenuItem";
+import SearchBox from "@/elements/layout/SearchBox";
 
 const Header = () => {
   return (
     <div className="flex justify-between items-center bg-neutral-50 py-2 px-4 my-4 border-2 border-neutral-700 rounded-md">
-      <div>
+      <div className="flex items-center gap-2">
         <ul className="flex items-center gap-0.5">
           <MenuItem href="/" title="خانه" />
           <MenuItem href="/property-files" title="آگهی‌ها" />
           <MenuItem href="/" title="درباره سایت" />
         </ul>
+        <SearchBox />
       </div>
     </div>
   );

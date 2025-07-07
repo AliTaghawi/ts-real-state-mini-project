9274164e9ac432d39753c40067e724da79fffb4c
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-08 00:56:34 +0330

    creating login api (POST) and developing it

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,30 @@
-import { NextRequest } from "next/server";
+import RSUser from "@/models/RSUser";
+import { StatusCodes, StatusMessages } from "@/types/enums";
+import { hashPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  
+  try {
+    await connectDB();
+
+    const { email, password, confirmPassword } = await req.json();
+
+    // validation
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = await RSUser.create({ email, password: hashedPassword });
+
+    return NextResponse.json(
+      { message: StatusMessages.USER_CREATED },
+      { status: StatusCodes.CREATED }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: StatusMessages.SERVER_ERROR },
+      { status: StatusCodes.SERVER_ERROR }
+    );
+  }
 }

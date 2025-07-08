bd95eddef9ca2aefc9262ab06cac25f1b85be988
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-09 03:29:01 +0330

    developing fomik validation in login page

--- components/templates/LoginPage.tsx
+++ components/templates/LoginPage.tsx
@@ -1,10 +1,36 @@
+"use client";
+
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { LoginType } from "@/types/types";
+import LoginRegisterForm from "@/modules/LoginRegisterForm";
+
+const initialValues: LoginType = {
+  email: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("لطفا ایمیل معتبر وارد کنید").required("الزامی!"),
+  password: Yup.string()
+    .min(8, "حداقل کارکتر مجاز 8 کارکتر میباشد")
+    .required("الزامی!"),
+});
 
 const LoginPage = () => {
-  return (
-    <div>
-      LoginPage
-    </div>
-  );
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+  });
+
+  async function onSubmit(
+    values: object,
+    { resetForm }: { resetForm: () => void }
+  ) {
+    console.log(values)
+  }
+  return <LoginRegisterForm formik={formik} type="login" />;
 };
 
 export default LoginPage;

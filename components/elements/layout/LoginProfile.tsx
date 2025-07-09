0521852c900f889cf953c15edb0d40e6083e2d70
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-09 04:02:52 +0330

    developing and completing loginProfile element base on session

--- components/elements/layout/LoginProfile.tsx
+++ components/elements/layout/LoginProfile.tsx
@@ -1,16 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 import { TbLogin2 } from "react-icons/tb";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const LoginProfile = () => {
+  const session = useSession();
   return (
     <div>
-      <Link
-        href="/login"
-        className="flex items-center gap-0.5 px-2 py-0.5 hover:bg-sky-200 rounded-sm transition-all ease-linear"
-      >
-        <TbLogin2 className="text-xl" />
-        Login
-      </Link>
+      {session.status === "authenticated" ? (
+        <Link href="/dashboard" className="text-2xl">
+          <FaRegUserCircle />
+        </Link>
+      ) : (
+        <Link
+          href="/login"
+          className="flex items-center gap-0.5 px-2 py-0.5 hover:bg-sky-200 rounded-sm transition-all ease-linear"
+        >
+          <TbLogin2 className="text-xl" />
+          Login
+        </Link>
+      )}
     </div>
   );
 };
--- components/elements/layout/SearchBox.tsx
+++ components/elements/layout/SearchBox.tsx
@@ -2,7 +2,7 @@ import { IoMdSearch } from "react-icons/io";
 
 const SearchBox = () => {
   return (
-    <div className="flex items-center gap-1 bg-neutral-50 py-0.5 px-2.5 rounded-2xl">
+    <div className="flex items-center gap-1 border-2 border-sky-300 bg-neutral-50 py-0.5 px-2.5 rounded-2xl">
       <IoMdSearch className="text-xl" />
       <input type="text" placeholder="جستجو در عنوان" className=" outline-0 w-[200px]" />
     </div>

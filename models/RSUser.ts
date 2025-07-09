46c9cf51f6396cfdb0f0f40b5b53fad23c3d41ad
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-09 05:18:54 +0330

    adding timestamps to User model

--- models/RSUser.ts
+++ models/RSUser.ts
@@ -1,23 +1,26 @@
-import { Schema, model, models} from 'mongoose'
+import { Schema, model, models } from "mongoose";
 
-const rsUserSchema = new Schema({
-  email: {
-    type: String,
-    required: true
-  }, 
-  password: {
-    type: String,
-    required: true
-  }, 
-  name: String,
-  lastName: String,
-  role: {
-    type: String,
-    enum: ["user", "subadmin", "admin"],
-    default: "user"
+const rsUserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: String,
+    lastName: String,
+    role: {
+      type: String,
+      enum: ["user", "subadmin", "admin"],
+      default: "user",
+    },
   },
-})
+  { timestamps: true }
+);
 
-const RSUser = models.RSUser || model("RSUser", rsUserSchema)
+const RSUser = models.RSUser || model("RSUser", rsUserSchema);
 
-export default RSUser
+export default RSUser;

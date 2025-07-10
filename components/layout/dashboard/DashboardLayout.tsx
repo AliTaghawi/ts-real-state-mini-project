b67b427994508cf674e63189bf6dae25df5f48d2
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-10 04:10:02 +0330

    making dashboard sidebar responsive

--- components/layout/dashboard/DashboardLayout.tsx
+++ components/layout/dashboard/DashboardLayout.tsx
@@ -11,33 +11,39 @@ import { ChildrenType } from "@/types/types";
 import { RootState } from "@/redux/stor";
 
 const linkStyle =
-  "flex gap-1 items-center py-0.5 px-1.5 border border-sky-400 w-11/12 justify-center mb-4 rounded-md hover:bg-sky-50";
+  "flex gap-1 items-center justify-center py-0.5 px-1.5 border border-sky-400 sm:w-11/12 sm:mb-4 rounded-md hover:bg-sky-50 text-xs sm:text-base";
 
 const DashboardLayout = ({ children }: ChildrenType) => {
   const user = useSelector((store: RootState) => store.user.user);
   return (
-    <div className="flex items-start gap-8">
-      <aside className="py-1.5 px-2.5 border-2 border-sky-400 rounded-lg min-w-[200px] flex flex-col items-center">
-        <div className="text-center">
-          <CgProfile className="text-5xl mt-4" />
-          {user?.role === "ADMIN" && <span className="text-sm">admin</span>}
+    <div className="flex flex-col sm:flex-row items-start gap-8">
+      <aside className="py-1.5 px-2.5 border-2 border-sky-400 rounded-lg w-full sm:w-[200px] flex sm:flex-col items-center gap-4 xs:gap-2.5 ">
+        <div className="flex items-center gap-2 sm:flex-col">
+          <div className="flex flex-col items-center">
+            <CgProfile className="sm:text-5xl text-lg sm:mt-4" />
+            {user?.role === "ADMIN" && (
+              <span className="sm:text-sm text-[10px]">admin</span>
+            )}
+          </div>
+          <h3 className="sm:mt-2 sm:mb-4 text-sm sm:text-md hidden min-[22.5rem]:inline-block">
+            {user?.showName}
+          </h3>
         </div>
-        <h3 className="mt-2 mb-4">{user?.showName}</h3>
         <Link href="/dashboard" className={linkStyle}>
-          <TbLayoutDashboardFilled className="text-sky-400 text-xl" />
-          <span>داشبورد</span>
+          <TbLayoutDashboardFilled className="text-sky-400 text-base sm:text-xl" />
+          <span className="hidden xs:inline-block">داشبورد</span>
         </Link>
         <Link href="/dashboard/add-file" className={linkStyle}>
-          <MdAddBox className="text-sky-400 text-xl" />
-          <span>ثبت آگهی</span>
+          <MdAddBox className="text-sky-400 text-base sm:text-xl" />
+          <span className="hidden xs:inline-block">ثبت آگهی</span>
         </Link>
         <Link href="/dashboard/profile" className={linkStyle}>
-          <RiProfileFill className="text-sky-400 text-xl" />
-          <span>حساب کاربری</span>
+          <RiProfileFill className="text-sky-400 text-base sm:text-xl" />
+          <span className="hidden xs:inline-block">حساب کاربری</span>
         </Link>
-        <button className="flex items-center gap-1 text-red-700 text-lg mb-5">
+        <button className="flex items-center gap-1 text-red-700 text-xs sm:text-lg sm:mb-5 mr-auto">
           <TbLogout className="text-xl" />
-          <span>خروج</span>
+          <span className="hidden sm:inline-block">خروج</span>
         </button>
       </aside>
       <main>{children}</main>

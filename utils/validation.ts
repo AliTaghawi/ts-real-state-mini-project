81325f89cdb44615327464c5d8aecc7a432ebfd7
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-15 04:29:44 +0330

    developing validation schema with joi for files

--- utils/validation.ts
+++ utils/validation.ts
@@ -52,4 +52,60 @@ const passwordUpdateSchema = Joi.object({
   }),
 });
 
-export { loginSchema, registerSchema, userSchema, passwordUpdateSchema };
+const fileValidationSchema = Joi.object({
+  title: Joi.string()
+    .required()
+    .messages({ "string.empty": `لطفا عنوان آگهی را وارد نمایید` }),
+  description: Joi.string()
+    .required()
+    .messages({ "string.empty": `لطفا توضیحات را کامل نمایید` }),
+  location: Joi.string()
+    .required()
+    .messages({ "string.empty": `لطفا موقعیت ملک را وارد نمایید` }),
+  address: Joi.string()
+    .required()
+    .messages({ "string.empty": `لطفا موقعیت ملک را وارد نمایید` }),
+  phone: Joi.string()
+    .regex(
+      /(((^(\+|00)(98)([- ]?))|^(0))(9\d{2})([- ]?)(\d{3})([- ]?)(\d{4})$)|((^(\+|00)(98)([- ]?))|^(0))([1-9]{2}[0-9]{8})$/
+    ) // iran num
+    .required()
+    .messages({
+      "string.empty": `لطفا شماره تلفن را وارد نمایید`,
+      "string.pattern.base": `شماره تلفن وارد شده صحیح نمی‌باشد`,
+    }),
+  realState: Joi.string()
+    .required()
+    .messages({ "string.empty": `لطفا نام بنگاه را وارد نمایید` }),
+  constructionDate: Joi.date()
+    .required()
+    .messages({ "date.empty": `لطفا تاریخ ساخت را وارد نمایید` }),
+  paymentType: Joi.string()
+    .regex(/rent|mortgage|buy/)
+    .required()
+    .messages({ "string.empty": `لطفا نوع ملک را مشخص کنید` }),
+  category: Joi.string()
+    .regex(/villa|office|store|apartment|land/)
+    .required()
+    .messages({ "string.empty": `لطفا نوع ملک را مشخص کنید` }),
+  price: [
+    Joi.number().required().messages({
+      "number.base": `قیمت باید با عدد نوشته شود`,
+      "number.empty": `لطفا قیمت را وارد نمایید`,
+    }),
+    Joi.object({
+      rent: Joi.number().required().messages({
+        "number.base": `مقدار کرایه باید با عدد نوشته شود`,
+        "number.empty": `لطفا مقدار کرایه را وارد نمایید`,
+      }),
+      mortgage: Joi.number().required().messages({
+        "number.base": `مقدار رهن باید با عدد نوشته شود`,
+        "number.empty": `لطفا مقدار رهن را وارد نمایید`,
+      }),
+    }),
+  ],
+  rules: Joi.array().items(Joi.string()),
+  amenities: Joi.array().items(Joi.string()),
+});
+
+export { loginSchema, registerSchema, userSchema, passwordUpdateSchema, fileValidationSchema };

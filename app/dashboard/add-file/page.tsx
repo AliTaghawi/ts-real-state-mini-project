d50fd3b6ae5820aa282b93b460161e83fa61fa60
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-16 21:43:03 +0330

    creating AddFile page

---
+++ app/dashboard/add-file/page.tsx
@@ -0,0 +1,9 @@
+import AddFilePage from "@/templates/AddFilePage";
+
+const AddFile = () => {
+  return (
+    <AddFilePage />
+  );
+};
+
+export default AddFile;
---
+++ components/templates/AddFilePage.tsx
@@ -0,0 +1,12 @@
+"use Client"
+
+const AddFilePage = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold">فرم ثبت آگهی</h2>
+      <div></div>
+    </div>
+  );
+};
+
+export default AddFilePage;

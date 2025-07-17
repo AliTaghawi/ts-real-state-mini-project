4f5f727e7ecaf438c0c9175e2d4571d2e0a23e52
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-17 12:57:37 +0330

    developing initialValues for formik

--- components/templates/AddFilePage.tsx
+++ components/templates/AddFilePage.tsx
@@ -1,6 +1,42 @@
-"use client"
+"use client";
 
 import AddFileForm from "@/modules/addFilePage/AddFileForm";
+
+interface initialValuesType {
+  title: string;
+  description: string;
+  location: string;
+  address: string;
+  realState: string;
+  phone: string;
+  fileType: "rent" | "mortgage" | "buy";
+  areaMeter: number;
+  price: number;
+  rent: number;
+  mortgage: number;
+  category: "villa" | "apartment" | "store" | "office" | "land";
+  constructionDate: Date;
+  amenities: string[];
+  rules: string[];
+}
+
+const initialValues: initialValuesType = {
+  title: "",
+  description: "",
+  location: "",
+  address: "",
+  realState: "",
+  phone: "",
+  fileType: "rent",
+  areaMeter: 30,
+  price: 0,
+  rent: 0,
+  mortgage: 0,
+  category: "apartment",
+  constructionDate: new Date(),
+  amenities: [],
+  rules: []
+};
 
 const AddFilePage = () => {
   return (

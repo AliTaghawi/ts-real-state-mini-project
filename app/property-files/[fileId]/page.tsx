ef6a756e57dc32b14fd6d505066d9363ac6fe30c
Ali Taghawi <marshaltaghawi@gmail.com>
2025-11-13 00:38:08 +0330

    creating FileDetails page and developing property-file/[fileId]

--- app/property-files/[fileId]/page.tsx
+++ app/property-files/[fileId]/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import connectDB from "@/utils/connectDB";
+import RSFile from "@/models/RSFile";
+import { notFound } from "next/navigation";
+import FileDetailsPage from "@/templates/FileDetailsPage";
+export const dynamicParams = true;
 
-const FileDetails = () => {
+export async function generateStaticParams() {
+  await connectDB();
+  const files = await RSFile.aggregate([
+    { $match: { published: true } },
+    { $project: { _id: 1 } },
+    { $limit: 2 }
+  ]);
+  return files.map((file: any) => ({ fileId: file._id.toString() }));
+}
+
+const FileDetails = async ({ params }: { params: Promise<{ fileId: string }> }) => {
+  const { fileId } = await params;
+  const file = await RSFile.findOne({ _id: fileId }).lean();
+  if (!file) notFound();
   return (
-    <div>
-      file Details
-    </div>
-  );
+    <FileDetailsPage file={file} />
+  )
 };
 
 export default FileDetails;
---
+++ components/templates/FileDetailsPage.tsx
@@ -0,0 +1,9 @@
+const FileDetailsPage = ({ file }: { file: any }) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default FileDetailsPage;
